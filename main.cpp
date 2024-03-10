95a9058496685119564740ae7766b1412f9200c7
LegendWasTaken <caiodcw@gmail.com>
2024-03-10 16:26:56 +0800

    2.6s

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <sys/mman.h>
 #include <unistd.h>
 #include <sys/stat.h>
+#include <bitset>
 #include "flat_hash_map.hpp"
 #include "concurrentqueue.h"
 
@@ -22,6 +23,19 @@ int parse_float(std::string_view input) {
             return ((input[0] - '0') * 10) + (input[2] - '0');
         }
     }
+}
+
+std::int32_t parse_float_v2(std::string_view input) {
+    auto in_word = std::int64_t();
+    std::memcpy(&in_word, input.data(), input.size());
+    const auto sign = ((in_word & 0xFF) ^ '-') == 0;
+    const auto first_shift = 8 * (input.size() - 1);
+    auto result = std::int32_t();
+    result += ((in_word >> (first_shift)) & 0xFF) - '0';
+    result += (((in_word >> (first_shift - 16)) & 0xFF) - '0') * 10;
+    result += ((((in_word >> (first_shift - 24)) & 0xFF) - '0') * 100) * (1 - (sign && first_shift != 32));
+    result -= 2 * result * sign;
+    return result;
 }
 
 struct data_entry {
@@ -62,35 +76,47 @@ void output_batch(std::set<std::string> &names, std::vector<data_entry> &data) {
     std::cout << '}';
 }
 
-void read_lines(std::string path, moodycamel::ConcurrentQueue<std::string_view> &queue, std::atomic<bool> &running) {
+struct line_read_boundaries {
+    int fd = 0;
+    size_t file_size = 0;
+    char *memory = nullptr;
+    std::vector<std::string_view> boundaries = {};
+
+    ~line_read_boundaries() {
+        munmap(memory, file_size);
+        close(fd);
+    }
+};
+line_read_boundaries read_lines(std::string path, std::uint32_t thread_count) {
     int fd = open(path.c_str(), O_RDONLY);
     struct stat sb;
-    size_t file_cursor = 0;
     fstat(fd, &sb);
     size_t file_size = sb.st_size;
     char* file_content = static_cast<char*>(mmap(NULL, file_size, PROT_READ, MAP_PRIVATE, fd, 0));
 
-    const size_t buffer_size = 13000;
+    auto boundaries = line_read_boundaries();
+    boundaries.fd = fd;
+    boundaries.file_size = file_size;
+    boundaries.memory = file_content;
 
-    while (file_cursor != file_size) {
-        const size_t remaining = file_size - file_cursor;
-        auto end = std::min(buffer_size, remaining) + file_cursor;
-        while (file_content[--end] != '\n');
-        queue.enqueue({file_content + file_cursor, file_content + end});
-        file_cursor = end + 1;
+    size_t file_cursor = 0;
+    const auto rough_estimate = file_size / thread_count;
+    for (size_t i = 0; i < thread_count - 1; i++) {
+        auto index = file_cursor + rough_estimate;
+        while (file_content[--index] != '\n');
+        boundaries.boundaries.emplace_back(file_content + file_cursor, file_content + index);
+        file_cursor = index + 1;
     }
+    boundaries.boundaries.emplace_back(file_content + file_cursor, file_content + file_size);
 
-    running = false;
-    munmap(file_content, file_size);
-    close(fd);
+    return boundaries;
 }
 
 std::vector<std::thread> dispatch_to_threads(
         std::set<std::string> &names,
         std::vector<std::vector<data_entry>> &entries,
-        moodycamel::ConcurrentQueue<std::string_view> &queue,
-        std::uint32_t thread_count,
-        std::atomic<bool> &running) {
+        line_read_boundaries &tasks,
+        std::uint32_t thread_count) {
     auto threads = std::vector<std::thread>();
 
     for (auto i = 0; i < thread_count; i++) {
@@ -100,24 +126,20 @@ std::vector<std::thread> dispatch_to_threads(
     for (std::uint32_t i = 0; i < thread_count; i++) {
         threads.emplace_back([&, i] () {
             auto &stats = entries[i];
-            auto lines = std::string();
-            while (running) {
-                if (queue.try_dequeue(lines)) {
-                    auto current = lines.begin();
-                    for (size_t j = 0; j < lines.size(); j++) {
-                        if (lines[j] == '\n') {
-                            const auto line = std::string_view(current, lines.begin() + j);
-                            auto semicolon = size_t(line.size());
-                            while (line[--semicolon] != ';');
-                            const auto name = std::string_view(line.begin(), line.begin() + semicolon);
-                            if (i == 0 && names.size() != 413) {
-                                names.insert(std::string(name));
-                            }
-                            const auto measurement = parse_float({line.begin() + semicolon + 1, line.end()});
-                            stats[index_from_name(name)].accumulate(measurement);
-                            current = lines.begin() + j + 1;
-                        }
+            const auto lines = tasks.boundaries[i];
+            auto current = lines.begin();
+            for (size_t j = 0; j < lines.size(); j++) {
+                if (lines[j] == '\n') {
+                    const auto line = std::string_view(current, lines.begin() + j);
+                    auto semicolon = size_t(line.size());
+                    while (line[--semicolon] != ';');
+                    const auto name = std::string_view(line.begin(), line.begin() + semicolon);
+                    if (i == 0 && names.size() != 413) {
+                        names.insert(std::string(name));
                     }
+                    const auto measurement = parse_float_v2({line.begin() + semicolon + 1, line.end()});
+                    stats[index_from_name(name)].accumulate(measurement);
+                    current = lines.begin() + j + 1;
                 }
             }
         });
@@ -126,23 +148,22 @@ std::vector<std::thread> dispatch_to_threads(
     return threads;
 }
 
+// 11001100101110001100100011000100000000
+// 11001100101110001100100011000100101101
+
+
 int main() {
     const auto thread_count = std::thread::hardware_concurrency();
 
     auto names = std::set<std::string>();
     auto stats = std::vector<data_entry>(32'768);
     auto entries = std::vector<std::vector<data_entry>>();
-    auto queue = moodycamel::ConcurrentQueue<std::string_view>();
-    auto running = std::atomic<bool>(true);
 
-    auto producer = std::thread([&](){
-        read_lines("measurements_large.txt", queue, running);
-    });
+    auto lines = read_lines("measurements_large.txt", thread_count);
 
-    for (auto &thread : dispatch_to_threads(names, entries, queue, thread_count, running)) {
+    for (auto &thread : dispatch_to_threads(names, entries, lines, thread_count)) {
         thread.join();
     }
-    producer.join();
 
     for (const auto &name : names) {
         const auto index = index_from_name(name);
